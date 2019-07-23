5571d397935651ce8474f87912baade45e685ff1
zhouxiaohui123 <663619355@qq.com>
2019-07-23 09:31:13 +0800

    test

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,6 +18,7 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+	 //测试提交
 	public function index()
 	{
 		$this->load->view('welcome_message');
